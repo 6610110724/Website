37999406073eb382f05987376b4ba84845c4b891
PiriPiri11 <153788607+6610110724@users.noreply.github.com>
2024-02-09 04:55:14 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -37,12 +37,12 @@ def tactics5():
 def tactics3():
     tactics_data = [
         {'name': '3 Defenders Formations'},
-        {'description': 'The 3 defender tactics are tactic that focuses on the defensive and uses ball possession in midfield to create scoring chances.'},
-        {'name': '3-4-3', 'description': 'This is a classic formation with 4 defenders, 4 midfielders, and 2 forwards.'},
-        {'name': '3-4-1-2', 'description': 'This formation that apply from 4-3-3 but 4-3-1-2 has 1 playmaker and 2 strikers.'},
-        {'name': '3-5-2', 'description': 'This formation apply from 4-4-2 but 4-2-4 push both wingers higher to focus on full attack.'},
-        {'name': '3-2-3-2', 'description': 'This formation apply from 4-4-2 but 4-2-3-1 will drop 1 striker down to be a playmaker.'},
-        {'name': '3-3-3-1', 'description': 'This is a modern formation with 4 defenders, 3 midfielders, 2 wingers forward and 1 target forward.'},
+        {'description': 'The 3 defender tactics are tactic that focuses on the offensive and uses ball possession in midfield to create scoring chances.'},
+        {'name': '3-4-3', 'description': 'This is a classic formation with 3 defenders, 4 midfielders, and 3 forwards.'},
+        {'name': '3-4-1-2', 'description': 'This formation that apply from 3-4-3 but 3-4-1-2 has 1 playmaker and 2 strikers.'},
+        {'name': '3-5-2', 'description': 'This formation apply from 3-4-3 and 3-4-1-2 but 3-5-2 drop 1 attacking player down to be a midfielders for more compact in the midfield.'},
+        {'name': '3-2-3-2', 'description': 'This formation apply from 3-5-2 but 3-2-3-2 will drop 2 midfielders down to be a defensive midfielder to increase tightness in defensive.'},
+        {'name': '3-3-2-1-1', 'description': 'This is a modern formation with 3 defenders, 3 midfielders, 2 playmakers, 1 deep-lying forward and 1 target forward.'},
     ]
     return render_template('tactics3.html', tactics=tactics_data)
 
