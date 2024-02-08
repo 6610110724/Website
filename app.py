31b149b5643921c0c2a0b1bc43b7a21f15a63b32
PiriPiri11 <153788607+6610110724@users.noreply.github.com>
2024-02-09 02:51:43 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -37,12 +37,13 @@ def tactics3():
         {'name': '3 Defenders Formations'},
         {'description': 'The 3 defender tactics are tactic that focuses on the defensive and uses ball possession in midfield to create scoring chances.'},
         {'name': '3-4-3', 'description': 'This is a classic formation with 4 defenders, 4 midfielders, and 2 forwards.'},
-        {'name': '3-5-2', 'description': 'This formation apply from 4-4-2 but 4-2-4 push both wingers higher to focus on full attack.'},
-        {'name': '3-2-3-3', 'description': 'This formation apply from 4-4-2 but 4-2-3-1 will drop 1 striker down to be a playmaker.'},
-        {'name': '3-3-3-1', 'description': 'This is a modern formation with 4 defenders, 3 midfielders, 2 wingers forward and 1 target forward.'},
         {'name': '3-4-1-2', 'description': 'This formation that apply from 4-3-3 but 4-3-1-2 has 1 playmaker and 2 strikers.'},
+        {'name': '3-5-2', 'description': 'This formation apply from 4-4-2 but 4-2-4 push both wingers higher to focus on full attack.'},
+        {'name': '3-2-3-2', 'description': 'This formation apply from 4-4-2 but 4-2-3-1 will drop 1 striker down to be a playmaker.'},
+        {'name': '3-3-3-1', 'description': 'This is a modern formation with 4 defenders, 3 midfielders, 2 wingers forward and 1 target forward.'},
     ]
     return render_template('tactics3.html', tactics=tactics_data)
+
 
 
 if __name__ == '__main__':
