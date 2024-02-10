c21b141699bec065a1f6ded288644e5ff53304f8
PiriPiri11 <153788607+6610110724@users.noreply.github.com>
2024-02-10 18:02:40 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -5,39 +5,35 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/supportus')
+def supportus():
+    return render_template('supportus.html')
+
 @app.route('/tactics4')
 def tactics4():
-    tactics_data = [
+    tactics_database = [
         {'name': '4 Defenders Formations', 'description': 'The 4 defender tactics are very popular tactic because tactical balance in both defensive and offensive.'},
-        {'name': '4-4-2', 'description': 'This is a classic formation with 4 defenders, 4 midfielders, and 2 forwards.'},
-        {'name': '4-2-3-1', 'description': 'This formation apply from 4-4-2 but 4-2-3-1 will drop 1 striker down to be a playmaker.'},
-        {'name': '4-3-3', 'description': 'This is a modern formation with 4 defenders, 3 midfielders, 2 wingers forward and 1 target forward.'},
-        {'name': '4-3-1-2', 'description': 'This formation that apply from 4-3-3 but 4-3-1-2 has 1 playmaker and 2 strikers.'},
     ]
-    return render_template('tactics4.html', tactics=tactics_data)
+    return render_template('tactics4.html', tactics=tactics_database)
 
 
 @app.route('/tactics5')
 def tactics5():
-    tactics_data = [
+    tactics_database = [
         {'name': '5 Defenders Formations', 'description': "The 5 defender tactics are tactic that focuses on defending and using the player's individual abilities to attack."},
-        {'name': '5-2-3', 'description': 'This formation focuses on defending and counter attack with both wingers.'},
-        {'name': '5-2-1-2', 'description': 'This formation apply from 5-2-3 but 5-2-1-2 will drop 1 striker down to be a playmaker.'},
-        {'name': '5-3-2', 'description': 'This formation apply from 5-2-3 and 5-2-1-2 but 5-3-2 will drop 1 attacking player down to be a midfielders for more compact in the midfield.'},
-        {'name': '5-4-1', 'description': 'This formation main focuses on defending but still managed ball prosession in the midfield with four midfielders.'},
     ]
-    return render_template('tactics5.html', tactics=tactics_data)
+    return render_template('tactics5.html', tactics=tactics_database)
 
 @app.route('/tactics3')
 def tactics3():
-    tactics_data = [
+    tactics_database = [
         {'name': '3 Defenders Formations', 'description': 'The 3 defender tactics are tactic that focuses on the offensive and uses ball possession in midfield to create scoring chances.'},
-        {'name': '3-4-3', 'description': 'This is a classic formation with 3 defenders, 4 midfielders, and 3 forwards.'},
-        {'name': '3-4-1-2', 'description': 'This formation that apply from 3-4-3 but 3-4-1-2 has 1 playmaker and 2 strikers.'},
-        {'name': '3-5-2', 'description': 'This formation apply from 3-4-3 and 3-4-1-2 but 3-5-2 drop 1 attacking player down to be a midfielders for more compact in the midfield.'},
-        {'name': '3-2-3-2', 'description': 'This formation apply from 3-5-2 but 3-2-3-2 will drop 2 midfielders down to be a defensive midfielder to increase tightness in defensive.'},
     ]
-    return render_template('tactics3.html', tactics=tactics_data)
+    return render_template('tactics3.html', tactics=tactics_database)
 
 if __name__ == '__main__':
     app.run(debug=True)
