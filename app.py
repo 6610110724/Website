d31261a3e535ea01d3afd89cc4067c7b5d18cee5
PiriPiri11 <153788607+6610110724@users.noreply.github.com>
2024-02-11 09:06:59 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -16,24 +16,35 @@ def supportus():
 @app.route('/tactics4')
 def tactics4():
     tactics_database = [
-        {'name': '4 Defenders Formations', 'description': 'The 4 defender tactics are very popular tactic because tactical balance in both defensive and offensive.'},
+        {'name': '4 Defenders Formations', 'description': 'The 4 defender tactics are very popular tactic because tactical balance in both defensive and offensive.  So, you can study about tactics below.'},
     ]
     return render_template('tactics4.html', tactics=tactics_database)
-
 
 @app.route('/tactics5')
 def tactics5():
     tactics_database = [
-        {'name': '5 Defenders Formations', 'description': "The 5 defender tactics are tactic that focuses on defending and using the player's individual abilities to attack."},
+        {'name': '5 Defenders Formations', 'description': "The 5 defender tactics are tactic that focuses on defending and using the player's individual abilities to attack.  So, you can study about tactics below."},
     ]
     return render_template('tactics5.html', tactics=tactics_database)
 
 @app.route('/tactics3')
 def tactics3():
     tactics_database = [
-        {'name': '3 Defenders Formations', 'description': 'The 3 defender tactics are tactic that focuses on the offensive and uses ball possession in midfield to create scoring chances.'},
+        {'name': '3 Defenders Formations', 'description': 'The 3 defender tactics are tactic that focuses on the offensive and uses ball possession in midfield to create scoring chances. So, you can study about tactics below.'},
     ]
     return render_template('tactics3.html', tactics=tactics_database)
+
+@app.route('/f343')
+def f343():
+    return render_template('f343.html')
+
+@app.route('/f3412')
+def f3412():
+    return render_template('f3412.html')
+
+@app.route('/f352')
+def f352():
+    return render_template('f352.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
